fd0d8bf509d879b6f2addc24752a06255b229bb3
Vlad <Vlad.V.Kuznetsov@hotmail.com>
2019-01-17 21:44:59 +0200

    the end

--- c_lab15/Main.c
+++ c_lab15/Main.c
@@ -3,27 +3,23 @@
 #include <string.h>
 #pragma warning(disable: 4996)
 
-
 int main()
 {
 	int n = 0;
 	scanf("%d", &n);
 
-	char modificator[] = "";
-	int x = -1000000001;
-	int mas[1000];
-
-	for (int i = 0; i < 100; i++)
-	{
-		mas[i] = x;
-	}
+	char modificator[100];
+	int x = -2147483648;
+	
+	int mas[10000];
+	for (int i = 0; i < sizeof(mas)/4; i++)
+		mas[i] = -2147483648;
 
 	if (n > 0 && n <= 100000)
 	{
-		
-
 		for (int i = 0; i < n; i++)
 		{
+		
 			scanf("%s", &modificator);
 
 			if (strcmp(modificator, "CONST") == 0)
@@ -31,9 +27,9 @@ int main()
 				scanf("%i", &x);
 				if (x < 1000000000 && x > -1000000000)
 				{
-					for (int i = 0; i < 1000; i++)
+					for (int i = 0; i < sizeof(mas); i++)
 					{
-						if (mas[i] == -1000000001)
+						if (mas[i] == -2147483648)
 						{
 							mas[i] = x;
 							break;
@@ -43,81 +39,81 @@ int main()
 			}
 			else if (strcmp(modificator, "ADD") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						mas[i - 2] += mas[i - 1];
-						mas[i - 1] = -1000000001;
+						mas[i - 1] = -2147483648;
 						break;
 					}
 				}
 			}
 			else if (strcmp(modificator, "SUB") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						mas[i - 2] = mas[i - 1] - mas[i - 2];
-						mas[i - 1] = -1000000001;
+						mas[i - 1] = -2147483648;
 						break;
 					}
 				}
 			}
 			else if (strcmp(modificator, "MUL") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						mas[i - 2] *= mas[i - 1];
-						mas[i - 1] = -1000000001;
+						mas[i - 1] = -2147483648;
 						break;
 					}
 				}
 			}
 			else if (strcmp(modificator, "DIV") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						mas[i - 2] = mas[i - 1] / mas[i - 2];
-						mas[i - 1] = -1000000001;
+						mas[i - 1] = -2147483648;
 						break;
 					}
 				}
 			}
 			else if (strcmp(modificator, "MAX") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						if (mas[i - 1] > mas[i - 2]) mas[i - 2] = mas[i - 1];
-						mas[i - 1] = -1000000001;
+						mas[i - 1] = -2147483648;
 						break;
 					}
 				}
 			}
 			else if (strcmp(modificator, "MIN") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						if (mas[i - 1] < mas[i - 2]) mas[i - 2] = mas[i - 1];
-						mas[i - 1] = -1000000001;
+						mas[i - 1] = -2147483648;
 						break;
 					}
 				}
 			}
 			else if (strcmp(modificator, "NEG") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						mas[i - 1] = -mas[i - 1];
 						break;
@@ -126,9 +122,9 @@ int main()
 			}
 			else if (strcmp(modificator, "DUP") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						mas[i] = mas[i - 1];
 						break;
@@ -137,14 +133,14 @@ int main()
 			}
 			else if (strcmp(modificator, "SWAP") == 0)
 			{
-				for (int i = 0; i < 1000; i++)
+				for (int i = 0; i < sizeof(mas); i++)
 				{
-					if (mas[i] == -1000000001)
+					if (mas[i] == -2147483648)
 					{
 						x = mas[i - 2];
 						mas[i - 2] = mas[i - 1];
 						mas[i - 1] = x;
-						x = -1000000001;
+						x = -2147483648;
 						break;
 					}
 				}
